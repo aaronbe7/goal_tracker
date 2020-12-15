207213e0f7d5fc70c7fad5630a32aec1e9a2f526
kylelainez <kyleadrianlainez@gmail.com>
2020-12-15 15:09:06 -0800

    Created Urls and Views methods

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+class CreateList(CreateView):
+    # model = GoalList
+    fields = '__all__'
+
+class UpdateList(UpdateView):
+    # model = GoalList
+    fields = '__all__'
+
+class DeleteList(DeleteView):
+    # model = GoalList
+    success_url = '/goals/'
+
+class GoalsList(ListView):
+    # model = GoalList
+    print('Placeholder')
+
+class GoalListDetails(DetailView):
+    # model = GoalList
+    print('Placeholder')
+
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'main_app/home.html')
+
+def user_goals(request, user_id):
+    return render(request, 'main_app/user_goals.html')
+
+def add_goal(request, user_id, list_id):
+    return render(request, 'main_app/add_goal')
