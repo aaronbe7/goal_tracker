5287b917f9e6d368802968c45bf693a73688ae3f
kylelainez <kyleadrianlainez@gmail.com>
2020-12-16 09:37:31 -0800

    Fix views file

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .models import GoalList
 
 # Create your views here.
 class CreateList(CreateView):
-    # model = GoalList
+    model = GoalList
     fields = '__all__'
 
 class UpdateList(UpdateView):
-    # model = GoalList
+    model = GoalList
     fields = '__all__'
 
 class DeleteList(DeleteView):
-    # model = GoalList
+    model = GoalList
     success_url = '/goals/'
 
 class GoalsList(ListView):
-    # model = GoalList
+    model = GoalList
     print('Placeholder')
 
 class GoalListDetails(DetailView):
-    # model = GoalList
+    model = GoalList
     print('Placeholder')
 
 def home(request):
@@ -30,4 +31,4 @@ def user_goals(request, user_id):
     return render(request, 'main_app/user_goals.html')
 
 def add_goal(request, user_id, list_id):
-    return render(request, 'main_app/add_goal')
+    return render(request, 'main_app/add_goal.html')
