b0048aa0d2ba4a73fe98689edc16075b75efcb7b
kylelainez <kyleadrianlainez@gmail.com>
2020-12-17 11:51:27 -0800

    Change Goal Link, Goal Detail View

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('user/<int:user_id>/goallist/<int:pk>/', views.GoalListDetail.as_view(), name='goallist_detail'),
     path('goals/create', views.GoalCreate.as_view(), name="create_goal"),
     # path('user/<int:user_id>/goals/<int:pk>/update/', views.UpdateGoal.as_view(), name='goal_update'),
-    # path('user/<int:user_id>/goals/<int:pk>/delete/', views.DeleteGoal.as_view(), name='goal_delete'),                
-    path('goals/<int:pk>/', views.GoalDetail.as_view(), name='goals_detail'),
+    # path('user/<int:user_id>/goals/<int:pk>/delete/', views.DeleteGoal.as_view(), name='goal_delete'),
+    path('user/<int:user_id>/goallist/<int:goallist_id>/goals/<int:pk>/', views.GoalDetail.as_view(), name='goals_detail'),
     path('user/<int:user_id>/goals/<int:list_id>/add_goal', views.add_goal, name="add_goal"),
 ]
