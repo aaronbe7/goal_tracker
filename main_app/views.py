1b35ad5f535c83c2f11f28895c36b6c673ff0185
kylelainez <kyleadrianlainez@gmail.com>
2020-12-22 14:34:59 -0800

    Fix Redirect of Sign Up

--- main_app/views.py
+++ main_app/views.py
@@ -195,7 +195,7 @@ def signup(request):
       user = form.save()
       # This is how we log a user in via code
       login(request, user)
-      return redirect('goal_list')
+      return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
